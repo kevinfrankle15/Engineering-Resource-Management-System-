79c9d491e511979f4d7f0a623dba5ecdef95edb9
kevinfrankle15 <kevinfrankle0000@gmail.com>
2025-06-08 13:33:10 +0530

    console production

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -4,6 +4,9 @@ import dotenv from "dotenv"
 
 dotenv.config()
 const isProduction = process.env.NODE_ENV === "production";
+
+console.log('Running in production?', isProduction);
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
