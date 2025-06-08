ff8509bdf304224b4b29346f4a767708585f37a1
kevinfrankle15 <kevinfrankle0000@gmail.com>
2025-06-08 13:37:27 +0530

    minor checks

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -1,13 +1,16 @@
 // src/db.ts
-import { Pool } from "pg"
-import dotenv from "dotenv"
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-dotenv.config()
-const isProduction = process.env.NODE_ENV === "production";
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 console.log('Running in production?', isProduction);
 
-export const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
-})
+});
+
+export default pool;
