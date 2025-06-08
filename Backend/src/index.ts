b55bfabf21a7709481d22b6b96c0e53b186efaeb
kevinfrankle15 <kevinfrankle0000@gmail.com>
2025-06-08 11:53:05 +0530

    log

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -25,6 +25,6 @@ app.get('/', (req, res) => {
 });
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-  console.log('Server running on http://localhost:5000');
+   console.log(`Server running on port ${port}`);
 });
 
